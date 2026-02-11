a86e0105690d49a0cfc3ee05237621ac1eb47968
Rad <99306685+Rad586@users.noreply.github.com>
2026-02-11 12:47:18 +0800

    Update DebugifyClient.java

--- src/client/java/dev/isxander/debugify/client/DebugifyClient.java
+++ src/client/java/dev/isxander/debugify/client/DebugifyClient.java
@@ -6,19 +6,7 @@ import dev.isxander.debugify.client.utils.BugFixDescriptionCache;
 public class DebugifyClient {
     public static BugFixDescriptionCache bugFixDescriptionCache;
 
-    public static void onInitializeClient() {
-        bugFixDescriptionCache = new BugFixDescriptionCache();
-
-        if (Debugify.configWasDirty) {
-            Debugify.LOGGER.info("Re-caching descriptions because json doesn't match config.");
-            bugFixDescriptionCache.cacheDescriptions();
-        } else {
-            if (!bugFixDescriptionCache.load()) {
-                Debugify.LOGGER.info("Failed to load descriptions, re-caching.");
-                bugFixDescriptionCache.cacheDescriptions();
-            }
-        }
-    }
+    public static void onInitializeClient() {}
 
     public static boolean isGameplayFixesEnabled() {
         return Debugify.CONFIG.gameplayFixesInMultiplayer;
